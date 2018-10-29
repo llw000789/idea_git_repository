1eb167da7bd72164bb4359e6c238636685c58bf2
zero <lingweizero@163.com>
2018-10-29 08:44:28 +0800

    左侧添加动作类

--- src/main/java/cn/itcast/controller/BaseController.java
+++ src/main/java/cn/itcast/controller/BaseController.java
@@ -3,7 +3,7 @@ package cn.itcast.controller;
 public class BaseController {
 
     public static void main(String[] args) {
-        System.out.println("测试提交后的还原");
+        System.out.println("测试提交后的还原11111");
     }
 
 }
